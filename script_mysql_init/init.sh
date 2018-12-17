b8764c62d5a8d59ed0356a2358eec244c36f49cd
hsgene_xu <wangxu@hsgene.com>
2018-12-17 10:39:11 +0800

    1

--- script_mysql_init/init.sh
+++ script_mysql_init/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # wx server
 echo "mysql check"
+sudo service mysql start
 mysql -uroot -p123123 -e "CREATE USER 'wx'@'%' IDENTIFIED BY 'rWk1hvqMT62K2JYH';"
 mysql -uroot -p123123 -e "CREATE DATABASE IF NOT EXISTS wx;"
 mysql -uroot -p123123 -e "GRANT ALL ON wx.* TO 'wx'@'%';"
