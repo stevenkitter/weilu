14ff2f8d82d10365a2268102764ab4588f574614
hsgene_xu <wangxu@hsgene.com>
2018-12-17 15:30:56 +0800

    ok

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker login -u 100000776220 -p wangxu226 ccr.ccs.tencentyun.com
 docker pull ccr.ccs.tencentyun.com/julu/weilu_wx
-docker-compose down && docker-compose up -d
+docker-compose down --remove-orphans && docker-compose up -d
