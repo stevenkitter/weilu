f10aaf19f384d9bf5b08583e70e2e27f1937e93e
hsgene_xu <wangxu@hsgene.com>
2018-12-18 10:52:42 +0800

    change weilu variables

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker login -u 100000776220 -p wangxu226 ccr.ccs.tencentyun.com
+docker login -u $DOCKER_TENCENT_USER -p $DOCKER_TENCENT_PASSWORD ccr.ccs.tencentyun.com
 docker pull ccr.ccs.tencentyun.com/julu/weilu_wx
 docker-compose down --remove-orphans && docker-compose up -d
